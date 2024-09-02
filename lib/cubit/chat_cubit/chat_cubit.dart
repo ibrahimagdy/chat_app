919a8038851b9229e511f2cf43d8c055a0736a45
ibrahimagdy <iibbrraahhimm00@gmail.com>
2024-09-03 01:51:49 +0300

    finish chat cubit

--- lib/cubit/chat_cubit/chat_cubit.dart
+++ lib/cubit/chat_cubit/chat_cubit.dart
@@ -1,6 +1,5 @@
 import 'package:bloc/bloc.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:meta/meta.dart';
 import 'package:scholar_chat/model/message_model.dart';
 
 part 'chat_state.dart';
@@ -10,21 +9,26 @@ class ChatCubit extends Cubit<ChatState> {
 
   CollectionReference messages =
       FirebaseFirestore.instance.collection('messages');
-
+  List<MessageModel> messageList = [];
   void sendMessage({required String data, required String email}) {
-    messages.add({
-      'message': data,
-      'CreatedAt': DateTime.now(),
-      'id': email,
-    });
+    try {
+      messages.add({
+        'message': data,
+        'CreatedAt': DateTime.now(),
+        'id': email,
+      });
+    } catch (e) {
+
+    }
   }
 
   void getMessage() {
-    List<MessageModel> messageList = [];
     messages.orderBy('CreatedAt', descending: true).snapshots().listen((e) {
+      messageList.clear();
         for (var doc in e.docs) {
           messageList.add(MessageModel.fromJson(doc));
         }
+        emit(ChatSuccess(messages: messageList));
       },
     );
   }
